37855be8072c4c7e58dde711da37aa5cafc428a8
Yelyzaveta Veis <liza.veis14@gmail.com>
2022-06-12 10:22:18 +0200

    development: - unnecessary api paths

--- tests/src/common/enums/api/api-path.enum.ts
+++ tests/src/common/enums/api/api-path.enum.ts
@@ -1,8 +1,5 @@
 enum ApiPath {
-  AUTH = '/auth/**',
   SIGN_IN = '/auth/sign-in',
-  SIGN_UP = '/auth/sign-up',
-  AUTHENTICATED_USER = '/auth/authenticated-user',
   TRIPS = '/trips',
   TRIPS_$ID = '/trips/**',
   BOOKINGS = '/bookings',
