89870cc35e7b349cbce4802a8a556fb010483cc5
StanislavVovk <62812681+StanislavVovk@users.noreply.github.com>
2022-07-16 22:31:56 +0300

    create react app, add router enum, create folders for components

---
+++ travel/src/components/App.jsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function App() {
+    return (
+        <div>
+            <span> "dasdadasdsadsajd"</span>
+        </div>
+    );
+}
+
+export default App;
---
+++ travel/src/enums/app-route-enum.ts
@@ -0,0 +1,9 @@
+const AppRoute: object = {
+    ROOT: "/",
+    SIGN_IN: "/sign-in",
+    SIGN_UP: "/sign-up",
+    BOOKINGS: "/bookings",
+    TRIP_$ID: "/trip/:tripId"
+}
+
+export default AppRoute
